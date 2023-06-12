0a27f3bbf595f2a5f37a3c942ef57b5130885a23
Xinyang Li <lix@lanl.gov>
2023-06-12 11:12:36 -0600

    update build script

--- openms/lib/build.sh
+++ openms/lib/build.sh
@@ -5,13 +5,13 @@ if [ ! -d "build" ]; then
 	mkdir build
 fi
 
-export EIGEN3_PATH=/path/to/eigen-3.3.9
+export EIGEN3_PATH=/Users/lix/codes/OpenMS/openms/lib/eigen-3.3.9
 
 cmake -B build \
 	-DENABLE_MPI=ON \
 	-DENABLE_MEEP=ON \
 	-DENABLE_TA=ON \
-	-DENABLE_TASCALAPACK=ON \
+	-DENABLE_TASCALAPACK=OFF \
 	-DENABLE_TACUDA=OFF \
 	-DENABLE_TAPYTHON=ON \
 	-DENABLE_TBB=ON \
